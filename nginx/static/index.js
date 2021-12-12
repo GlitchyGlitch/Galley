959b8abcd199c7a7738c14cca5ca44e6c8234d8b
Mateusz Jaśkiewicz <jaskiewiczteo@gmail.com>
2021-12-12 21:01:00 +0100

    Add subpages on frontend

--- nginx/static/index.js
+++ nginx/static/index.js
@@ -1,6 +1,39 @@
+"use strict";
+
 import Router from "./router.js";
 
-const router = new Router();
-router.add("", () => {
-  console.log("main_contorller");
-});
+const fetchView = async (viewPath) => {
+  const req = new Request(viewPath, {
+    method: "GET",
+  });
+
+  let response = await fetch(req);
+  response = await response.text();
+  const element = document.createElement("div");
+  element.innerHTML = response;
+  return element;
+};
+
+const main = () => {
+  const routerView = document.querySelector("[router-view]");
+
+  const router = new Router();
+
+  router
+    .add(/login/, async () => {
+      routerView.innerHTML = "";
+      const view = await fetchView("views/login.html");
+      routerView.append(view);
+    })
+    .add(/register/, async () => {
+      routerView.innerHTML = "";
+      const view = await fetchView("views/register.html");
+      routerView.append(view);
+    })
+    .add("", async () => {
+      routerView.innerHTML = "";
+      const view = await fetchView("views/home.html");
+      routerView.append(view);
+    });
+};
+document.addEventListener("DOMContentLoaded", main);
