4442f29410c9ac7e0eba82c6b71b5363b2b7fd9e
Mateusz Jaśkiewicz <jaskiewiczteo@gmail.com>
2022-03-01 10:41:16 +0100

    Fix comment bug

--- gallery/models/comment.php
+++ gallery/models/comment.php
@@ -23,7 +23,7 @@ class Comment extends Model implements ModelInterface
   }
   public function validate_content(): bool
   {
-    if (trim($this->content) != "" && !(strlen($this->content) > 510) && preg_match('/^[0-9a-zA-Ząćęłńóśźż!.?]+$/u', $this->content)) {
+    if (trim($this->content) != "" && !(strlen($this->content) > 510) && preg_match('/^[0-9a-zA-Ząćęłńóśźż!.,? ]+$/u', $this->content)) {
       return true;
     }
     return false;
